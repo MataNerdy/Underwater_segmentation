6a02ccaea309e0e1ab5418a393b219c4d34acf86
Nerdniwe <moontan.ln@gmail.com>
2026-05-22 20:23:13 +0400

    new script

--- scripts/run_kaggle_experiments.sh
+++ scripts/run_kaggle_experiments.sh
@@ -9,11 +9,12 @@ else
   RUN_SUFFIX="main"
 fi
 
-DATASET_NAME="${DATASET_NAME:-datasets/ashish2001/semantic-segmentation-of-underwater-imagery-suim}"
+DATASET_NAME="${DATASET_NAME:-semantic-segmentation-of-underwater-imagery-suim}"
 DATA_DIR="${DATA_DIR:-/kaggle/input/${DATASET_NAME}}"
 
-TRAIN_IMAGES_DIR="${TRAIN_IMAGES_DIR:-${DATA_DIR}/train/images}"
-TRAIN_MASKS_DIR="${TRAIN_MASKS_DIR:-${DATA_DIR}/train/masks}"
+TRAIN_IMAGES_DIR="${TRAIN_IMAGES_DIR:-${DATA_DIR}/train_val/images}"
+TRAIN_MASKS_DIR="${TRAIN_MASKS_DIR:-${DATA_DIR}/train_val/masks}"
+TEST_IMAGES_DIR="${TEST_IMAGES_DIR:-${DATA_DIR}/TEST/images}"
 
 WORKING_DIR="${WORKING_DIR:-/kaggle/working}"
 CHECKPOINT_DIR="${WORKING_DIR}/checkpoints"
